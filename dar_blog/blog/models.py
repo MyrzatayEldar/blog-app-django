85af762d3440bd519ce1f7308ceec4c778964c2f
MyrzatayEldar <eldar.myrza@gmail.com>
2022-06-22 14:47:32 +0600

    Настроил User модель

--- dar_blog/blog/models.py
+++ dar_blog/blog/models.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Info(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    age = models.IntegerField()
+class User(AbstractUser):
+    surname = models.CharField(max_length=255, verbose_name='Отчество')
+    image = models.ImageField(upload_to='user_pictures/')
--- dar_blog/blog/views.py
+++ dar_blog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'blog/login.html', {})
+
+
+def main(request):
+    return render(request, 'blog/main.html', {'a': 25})
