36d6d335b303ed752cf8d47eaa8bb0d248a74941
MyrzatayEldar <eldar.myrza@gmail.com>
2022-06-30 09:59:04 +0600

    Починил взаимодействие с базовым шаблоном.

--- dar_blog/blog/models.py
+++ dar_blog/blog/models.py
@@ -1,13 +1,34 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 
 class User(AbstractUser):
     surname = models.CharField(max_length=255, verbose_name='Отчество')
-    image = models.ImageField(upload_to='user_pictures/', verbose_name='Фото профиля')
+    image = models.ImageField(upload_to=f'user_pictures/{User.username}/', verbose_name='Фото профиля')
 
 
 class Post(models.Model):
-    author = models.OneToOneField('User')
-    published_date = models.DateTimeField(auto_now_add=True)
-    text = models.TextField()
+    author = models.OneToOneField('User', on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=255, verbose_name='Тема поста')
+    published_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата написания')
+    text = models.TextField(verbose_name='Текст поста')
+    category = models.ManyToManyField('Category', verbose_name='Категория')
+    image = models.ImageField(upload_to=f'post_pictures/{author}', verbose_name='Фото')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return f'Пост {self.author}. Дата написания: {self.published_date}.'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'Категория: {self.name}'
