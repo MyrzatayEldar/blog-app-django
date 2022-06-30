4d25ce3beea687fccfb66ba0b76080a33e737fee
MyrzatayEldar <eldar.myrza@gmail.com>
2022-06-30 23:26:26 +0600

    сделана форма регистрации, но кнопка регистрации пока не работает. займусь завтра.

--- dar_blog/blog/forms.py
+++ dar_blog/blog/forms.py
@@ -22,3 +22,45 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'surname', 'email',  'username', 'password1', 'password2', 'image', )
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'type': 'text',
+                'id': 'form3Example1c',
+                'class': 'form-control',
+                'placeholder': 'Ваше имя'}),
+            'last_name': forms.TextInput(attrs={
+                'type': 'text',
+                'id': 'last_name',
+                'class': 'form-control',
+                'placeholder': 'Ваша фамилия'}),
+            'surname': forms.TextInput(attrs={
+                'type': 'text',
+                'id': 'surname',
+                'class': 'form-control',
+                'placeholder': 'Ваше отчество'}),
+            'email': forms.TextInput(attrs={
+                'type': 'email',
+                'id': 'form3Example3c',
+                'class': 'form-control',
+                'placeholder': 'Ваша почта'}),
+            'username': forms.TextInput(attrs={
+                'type': 'text',
+                'id': 'username',
+                'class': 'form-control',
+                'placeholder': 'Имя пользователя'}),
+            'password1': forms.PasswordInput(attrs={
+                'type': 'password',
+                'id': 'form3Example4c',
+                'class': 'form-control',
+                'placeholder': 'Ваш пароль'}),
+            'password2': forms.PasswordInput(attrs={
+                'type': 'password',
+                'id': 'form3Example4cd',
+                'class': 'form-control',
+                'placeholder': 'Повторите пароль'}),
+            'image': forms.TextInput(attrs={
+                'type': 'file',
+                'id': 'file',
+                'class': 'form-control',
+                'placeholder': 'Фото профиля'}),
+        }
